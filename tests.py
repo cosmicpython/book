c953c7540098a3e65712e070a5bba745a303bca0
Harry <hjwp2@cantab.net>
2019-02-22 14:44:41 +0000

    progress, find filenames

--- tests.py
+++ tests.py
@@ -1,15 +1,19 @@
 from pathlib import Path
 from lxml import html
+import re
 
 def parse_listings(chapter_name):
-    raw_contents = Path(f'{chapter_name}.asciidoc').read_text()
+    raw_contents = Path(f'{chapter_name}.html').read_text()
     parsed_html = html.fromstring(raw_contents)
 
-    for listing_node in parsed_html.cssselect('.listingblock'):
-        [content_node] = listing_node.cssselect('.content')
-        yield content_node.text_content()
+    for listing_node in parsed_html.cssselect('.exampleblock'):
+        [content_node] = listing_node.cssselect('.listingblock .content pre')
+        [title_node] = listing_node.cssselect('.title')
+        filename = re.search(r'.+ \((.+)\)', title_node.text_content()).group(1)
+        yield filename, content_node.text_content()
+
 
 def test_chapter_01():
-    for l in parse_listings('chapter_01_domain_model'):
-        assert 'test' in l
+    for filename, listing in parse_listings('chapter_01_domain_model'):
+        assert 'test' in listing
 
