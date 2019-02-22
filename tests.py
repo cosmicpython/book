af3256298c202f250c52e3bda484ca92054d6123
Harry <hjwp2@cantab.net>
2019-02-22 15:07:20 +0000

    a good first cut of tests

--- tests.py
+++ tests.py
@@ -1,6 +1,15 @@
 from pathlib import Path
 from lxml import html
+import subprocess
 import re
+import difflib
+
+def test_chapter_01():
+    chapter = 'chapter_01_domain_model'
+    for filename, listing in parse_listings(chapter):
+        actual_contents = file_contents_for_branch(filename, chapter)
+        if listing not in actual_contents:
+            raise_with_helpful_diff(filename, listing, actual_contents)
 
 def parse_listings(chapter_name):
     raw_contents = Path(f'{chapter_name}.html').read_text()
@@ -13,7 +22,15 @@ def parse_listings(chapter_name):
         yield filename, content_node.text_content()
 
 
-def test_chapter_01():
-    for filename, listing in parse_listings('chapter_01_domain_model'):
-        assert 'test' in listing
+def file_contents_for_branch(filename, chapter_name):
+    return subprocess.run(
+        ['git', 'show', f'origin/{chapter_name}:{filename}'],
+        cwd=Path(__file__).parent / 'code',
+        stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        check=True
+    ).stdout.decode()
+
+
+def raise_with_helpful_diff(filename, listing, actual_contents):
+    assert listing.split('\n') == actual_contents.split('\n')
 
