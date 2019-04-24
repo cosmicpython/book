523952623d29d0f6750232b556459a62e4fd613d
Harry <hjwp2@cantab.net>
2019-04-24 12:33:34 +0100

    udpate submodules feed thru code

--- push-branches.py
+++ push-branches.py
@@ -9,4 +9,8 @@ for chapter in CHAPTERS:
         ['git', 'push', '--force-with-lease', 'origin', chapter],
         cwd=Path(__file__).parent / 'code'
     )
+subprocess.run(
+    ['git', 'push', '--force-with-lease', 'origin', 'master'],
+    cwd=Path(__file__).parent / 'code'
+)
 
