e63b51dec70c136d2f63bdb4067fb429bbf3dd21
Harry <hjwp2@cantab.net>
2019-11-15 16:38:20 +0000

    push branches in parallel

--- push-branches.py
+++ push-branches.py
@@ -4,22 +4,33 @@ import subprocess
 from pathlib import Path
 from chapters import CHAPTERS, NO_EXERCISE
 
+processes = []
+
 for chapter in CHAPTERS:
-    print('pushing', chapter, end=': ', flush=True)
-    subprocess.run(
-        ['git', 'push', '--force-with-lease', 'origin', chapter],
-        cwd=Path(__file__).parent / 'code'
-    )
+    print('pushing', chapter)
+    processes.append(subprocess.Popen(
+        ['git', 'push', '-v', '--force-with-lease', 'origin', chapter],
+        cwd=Path(__file__).parent / 'code',
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True,
+    ))
     if chapter in NO_EXERCISE:
         continue
     exercise_branch = f'{chapter}_exercise'
-    print('pushing', exercise_branch, end=': ', flush=True)
-    subprocess.run(
-        ['git', 'push', '--force-with-lease', 'origin', exercise_branch],
-        cwd=Path(__file__).parent / 'code'
-    )
+    print('pushing', exercise_branch)
+    processes.append(subprocess.Popen(
+        ['git', 'push', '-v', '--force-with-lease', 'origin', exercise_branch],
+        cwd=Path(__file__).parent / 'code',
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True,
+    ))
 
-subprocess.run(
-    ['git', 'push', '--force-with-lease', 'origin', 'master'],
-    cwd=Path(__file__).parent / 'code'
-)
+print('pushing master')
+processes.append(subprocess.Popen(
+    ['git', 'push', '-v', '--force-with-lease', 'origin', 'master'],
+    cwd=Path(__file__).parent / 'code',
+    stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True,
+))
+
+for p in processes:
+    stdout, stderr = p.communicate()
+    print(stdout)
+    print(stderr)
