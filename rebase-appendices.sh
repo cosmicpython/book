80938d25e3a168324c866732e01971cd4d3d82dc
Harry <hjwp2@cantab.net>
2020-01-14 12:44:28 +0000

    change all xrefs

--- rebase-appendices.sh
+++ rebase-appendices.sh
@@ -3,9 +3,9 @@ set -ex
 
 cd code
 git co appendix_django
-git irebase chapter_05_uow
+git irebase chapter_06_uow
 
 git co appendix_csvs
-git irebase chapter_05_uow
+git irebase chapter_06_uow
 
 git co master
--- renumber-chapters.py
+++ renumber-chapters.py
@@ -19,19 +19,34 @@ for frm, to in MOVES:
     subprocess.run(['git', 'mv', f'{frm}.asciidoc', f'{to}.asciidoc'], check=True)
 
 sources = list(Path(__file__).absolute().parent.glob('*.asciidoc'))
+otherthings = [
+    'chapters.py',
+    'atlas.json',
+    'Readme.md',
+    'rebase-chapters.sh',
+    'rebase-appendices.sh',
+]
 for frm, to in MOVES:
     subprocess.run(
-        ['sed', '-i', f's/{frm}/{to}/g'] + sources + ['chapters.py', 'atlas.json', 'Readme.md'],
+        ['sed', '-i', f's/{frm}/{to}/g'] + sources + otherthings,
         check=True,
     )
 
 input('base repo done, ready to do submodules')
 
-for frm, to in MOVES[1:]:
+for frm, to in MOVES:
     code = Path(__file__).absolute().parent / 'code'
     subprocess.run(['git', 'branch', '-m', frm, to], cwd=code)
+    subprocess.run(['git', 'branch', '--unset-upstream', to], cwd=code) # untested
     subprocess.run(['git', 'push', 'origin', f':{frm}'], cwd=code)
     subprocess.run(['git', 'checkout', to], cwd=code)
-    subprocess.run(['git', 'branch', '--unset-upstream'], cwd=code)
     subprocess.run(['git', 'push', '-u', 'origin', to], cwd=code)
+    from chapters import NO_EXERCISE
+    if to not in NO_EXERCISE:
+        # untested
+        subprocess.run(
+            ['git', 'branch', '-m', f'{frm}_exercise', f'{to}_exercise'],
+            cwd=code, check=True,
+        )
+
     input(f'{frm}->{to} done in theory')
