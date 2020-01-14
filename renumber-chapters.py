0a4bc9f082a316a14c107e39479e4cc131e8e42a
Harry <hjwp2@cantab.net>
2020-01-14 12:10:30 +0000

    update submodule now branches fixed

--- renumber-chapters.py
+++ renumber-chapters.py
@@ -4,13 +4,19 @@ from pathlib import Path
 
 MOVES = [
     # change these as desired
-    ('chapter_09B_external_events', 'chapter_10_external_events'),
-    ('chapter_10_cqrs', 'chapter_11_cqrs'),
-    ('chapter_11_dependency_injection', 'chapter_12_dependency_injection'),
+    ('chapter_04b_high_gear_low_gear', 'chapter_05_high_gear_low_gear'),
+    ('chapter_05_uow', 'chapter_06_uow'),
+    ('chapter_06_aggregate', 'chapter_07_aggregate'),
+    ("chapter_07_events_and_message_bus", "chapter_08_events_and_message_bus"),
+    ("chapter_08_all_messagebus", "chapter_09_all_messagebus"),
+    ("chapter_09_commands", "chapter_10_commands"),
+    ("chapter_10_external_events", "chapter_11_external_events"),
+    ("chapter_11_cqrs", "chapter_12_cqrs"),
+    ("chapter_12_dependency_injection", "chapter_13_dependency_injection"),
 ]
 
 for frm, to in MOVES:
-    subprocess.run(['git', 'mv', f'{frm}.asciidoc', f'{to}.asciidoc'])
+    subprocess.run(['git', 'mv', f'{frm}.asciidoc', f'{to}.asciidoc'], check=True)
 
 sources = list(Path(__file__).absolute().parent.glob('*.asciidoc'))
 for frm, to in MOVES:
